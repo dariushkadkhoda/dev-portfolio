61cd210f3015115466a2a5e1e3a3d23ccfaf5785
dariush kadkhoda <dariushkadkhoda@gmail.com>
2024-09-08 15:24:14 +0330

    home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,48 @@
-import Image from "next/image";
+import Photo from "@/components/Photo";
+import Socials from "@/components/Socials";
+import Status from "@/components/Status";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
-export default function Home() {
+
+const Home = () => {
   return (
-  <main className="">
-
-  </main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl-pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span>Software Developer</span>
+            <h1 className="text-[48px] xl:text[80px] leading-[1.1] font-semibold mb-6">
+              Hello I'm <br /> <span className="text-accent">Dariush Kadkhoda</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital expeiences and I am proficient
+              in various programing lenguges and technologies
+            </p>
+            {/* btn and social */}
+            <div className="flex flex-col xl:flex-row gap-8">
+              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                <span>Download CV</span>
+                <FiDownload className="text-xl"/>
+              </Button>
+              <div className="mb-8 xl:mb-0 flex justify-center items-center">
+                <Socials containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full
+                flex  items-center justify-center text-accent text-base hover:bg-accent hover:text-primary
+                hover:transition-all duration-500
+                "  />
+              </div>
+            </div>
+          </div>
+          {/* photo */}
+          <div className="order-1 cl:order-none mb-8 xl:mb-0">
+            <Photo/>
+          </div>
+        </div>
+      </div>
+      <Status/>
+    </section>
   );
-}
+};
+
+export default Home;
